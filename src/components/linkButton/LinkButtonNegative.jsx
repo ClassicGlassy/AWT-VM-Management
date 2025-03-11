077c8d0f64ec1de1e4deeac2e466ec14b3c9505c
ClassicGlassy <114240379+ClassicGlassy@users.noreply.github.com>
2025-03-11 11:53:55 +0545

    Add LinkButton types

---
+++ src/components/linkButton/LinkButtonNegative.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
+function LinkButtonNegative({ to, Icon, text }) {
+  return (
+    <Link
+      to={to}
+      className="py-2 px-5 text-white bg-negative hover:bg-negative-hover flex justify-center items-center rounded-md font-medium transition-colors ease-in-out duration-200"
+    >
+      <span className="inline-block text-2xl md:text-3xl">
+        <Icon />
+      </span>
+      <span className="hidden md:block ml-4">{text}</span>
+    </Link>
+  );
+}
+export default LinkButtonNegative;
